1725fcc44260b74e2dc5dceca7f272c0016c5bd9
Colin Burgess <crb5825@gmail.com>
2023-11-16 15:48:56 -0600

    App now requires notifications

--- app/src/main/java/com/app/followup/MainActivity.kt
+++ app/src/main/java/com/app/followup/MainActivity.kt
@@ -7,9 +7,9 @@ import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.AlertDialog
-import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
@@ -19,98 +19,90 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private var isAccessEnabled = mutableStateOf(false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FollowUpApp()
+            FollowUpApp(isAccessEnabled)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        isAccessEnabled.value = isNotificationServiceEnabled(this)
+    }
+
+    private fun isNotificationServiceEnabled(context: Context): Boolean {
+        val enabledNotificationListeners =
+            Settings.Secure.getString(context.contentResolver, "enabled_notification_listeners")
+        return enabledNotificationListeners?.contains(context.packageName) == true
     }
 }
 
 @Composable
-fun FollowUpApp() {
-    val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
+fun FollowUpApp(isAccessEnabled: MutableState<Boolean>) {
     val context = LocalContext.current
+    val scaffoldState = rememberScaffoldState()
 
     Scaffold(
         scaffoldState = scaffoldState,
         topBar = { TopAppBar(title = { Text("Follow Up") }) },
-        floatingActionButton = {
-            FloatingActionButton(onClick = {
-                scope.launch {
-                    scaffoldState.snackbarHostState.showSnackbar(
-                        message = "Replace with your own action",
-                        actionLabel = "Action"
-                    )
-                }
-            }) {
-                Text("Action")
+        floatingActionButton = { /* ... */ }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            if (!isAccessEnabled.value) {
+                ShowNotificationAccessDialog(context)
             }
         }
-    ) { innerPadding ->
-        NotificationAccessCheck(context, innerPadding)
-    }
-}
-
-@Composable
-fun NotificationAccessCheck(context: Context, innerPadding: PaddingValues) {
-    val isAccessEnabled = remember { isNotificationServiceEnabled(context) }
-
-    if (!isAccessEnabled.value) {
-        ShowNotificationAccessDialog(context)
     }
 }
 
 @Composable
 fun ShowNotificationAccessDialog(context: Context) {
-    val openDialog = remember { mutableStateOf(true) }
-
-    if (openDialog.value) {
-        AlertDialog(
-            onDismissRequest = {},
-            title = { Text("Notification Access Required") },
-            text = { Text("Notification access is required for Follow Up to function properly.") },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        openDialog.value = false
-                        val intent = Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
-                        context.startActivity(intent)
-                    }
-                ) {
-                    Text("Go to Settings")
+    AlertDialog(
+        // Do not allow dismissing by tapping outside the dialog.
+        onDismissRequest = {
+        },
+        title = { Text("Notification Access Required") },
+        text = { Text("Notification access is required for Follow Up to function properly.") },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    val intent = Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
+                    context.startActivity(intent)
                 }
-            },
-            dismissButton = {
-                TextButton(
-                    onClick = {
-                        openDialog.value = false
-                        (context as? Activity)?.finish()
-                    }
-                ) {
-                    Text("Exit App")
-                }
+            ) {
+                Text("Go to Settings")
             }
-        )
-    }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    (context as? Activity)?.finish()
+                }
+            ) {
+                Text("Exit App")
+            }
+        }
+    )
 }
 
-fun isNotificationServiceEnabled(context: Context): MutableState<Boolean> {
-    val enabledNotificationListeners =
-        Settings.Secure.getString(context.contentResolver, "enabled_notification_listeners")
-    return mutableStateOf(enabledNotificationListeners?.contains(context.packageName) == true)
-}
-
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Access Enabled")
 @Composable
-fun PreviewFollowUpApp() {
-    FollowUpApp()
+fun PreviewFollowUpAppAccessEnabled() {
+    val isAccessEnabled = remember { mutableStateOf(true) }
+    FollowUpApp(isAccessEnabled)
+}
+
+@Preview(showBackground = true, name = "Access Not Enabled")
+@Composable
+fun PreviewFollowUpAppAccessNotEnabled() {
+    val isAccessEnabled = remember { mutableStateOf(false) }
+    FollowUpApp(isAccessEnabled)
 }
